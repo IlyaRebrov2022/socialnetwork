fce9cb56cc566c5f0c8a6b710e20ae65bf3454f1
IlyaRebrov2022 <ilyarebrovkripta@gmail.com>
2022-04-25 16:25:32 +0300

    первый коммит

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://images.wallpaperscraft.ru/image/single/medved_risunok_zhivotnoe_87247_1280x1280.jpg'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='image'>
+                <div className='avatar'></div>
+            </div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
